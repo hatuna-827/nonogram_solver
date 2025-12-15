a5d92f8bbc362ef3561a8ae9b04e926f7261ec90
hatuna-827 <hatuna827yadaka@gmail.com>
2025-12-15 22:45:29 +0900

    add answer

---
+++ test.py
@@ -0,0 +1,136 @@
+# def fill_line(grid, Hb_sum, Hw, h):
+#     for i in range(1, len(Hw) - 1):
+#         Hw[i] += 1
+#     fill(grid, h, 0, h, Hw[0], ".")
+#     Hw_sum = [0]
+#     for i in range(len(Hw)):
+#         Hw_sum.append(Hw_sum[-1] + Hw[i])
+#     for i in range(1, len(Hw)):
+#         fill(grid, h, Hw_sum[i] + Hb_sum[i - 1], h, Hw_sum[i] + Hb_sum[i], "#")
+#         fill(grid, h, Hw_sum[i] + Hb_sum[i], h, Hw_sum[i + 1] + Hb_sum[i], ".")
+#     for i in range(1, len(Hw) - 1):
+#         Hw[i] -= 1
+#     return grid
+
+
+# def fill(grid, start_h, start_w, end_h, end_w, content):
+#     if start_w == -1:
+#         start_w = 0
+#     for h in range(start_h, end_h + 1):
+#         for w in range(start_w, end_w):
+#             grid[h][w] = content
+#     return grid
+
+
+# def print_out(grid):
+#     for line in grid:
+#         print(" ".join(line))
+#     return
+
+
+# d = [1, 1, 1]
+# width = 8
+
+# b_sum = [0]
+# for i in d:
+#     b_sum.append(b_sum[-1] + i)
+
+# result = []
+# list = [0] * (len(d) + 1)
+# list[-1] = width - sum(d) - len(d) + 1
+# result.append(list[:])
+# while list[0] != width - sum(d) - len(d) + 1:
+#     i = -1
+#     while list[i] == 0:
+#         i -= 1
+#     list[i - 1] += 1
+#     list[-1] = list[i] - 1
+#     if i != -1:
+#         list[i] = 0
+#     result.append(list[:])
+
+# print(result)
+# for i in range(len(result)):
+#     result[i] = fill_line([["?"] * width], b_sum, result[i], 0)[0]
+# print_out(result)
+
+# -------------------------------------------------------------------------------------------------------
+
+# d = [2, 1, 1]
+# width = 8
+
+# min = [0]
+# for i in d[:-1]:
+#     min.append(min[-1] + i + 1)
+
+# result=[]
+# print(min)
+
+# print([""]*width)
+# print([""]*(width-min[-1]))
+
+# -------------------------------------------------------------------------------------------------------
+
+def print_out(g):
+    for value in g:
+        for v in value:
+            print(v)
+
+hoge=[
+    "##.#.#..",
+    "##.#..#.",
+    "##.#...#",
+
+    "##..#.#.",
+    "##..#..#",
+
+    "##...#.#",
+
+    ".##.#.#.",
+    ".##.#..#",
+
+    ".##..#.#",
+
+    "..##.#.#"
+]
+# hoge.reverse()
+# for i in hoge:
+#     print(i)
+
+d = [2, 2, 1]
+width = 8
+
+min=[d[0]]
+for i in d[1:]:
+    min.append(min[-1]+i+1)
+d.reverse()
+max=[width]
+for i in d[:-1]:
+    max.insert(0,max[0]-i-1)
+d.reverse()
+
+result=[]
+for w in range(width):
+    result.append([])
+
+for i in range(len(d)):
+    for s in range(min[i]):
+        print(s)
+        index=s-d[i]
+        print(f"index:{index}")
+        for e in range(s+1):
+            result[index].append("."*e+"#"*d[i]+"."*(s-e))
+
+print(result)
+print_out(result)
+exit()
+
+# for i in d
+# for r in range(min,max)
+# for i in range(sum(d) - len(d) + 2):
+#     result.append("." * i + "#" * d[0])
+
+for i in range(len(d)):
+    print(i)
+
+print(result)
