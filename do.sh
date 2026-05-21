75c4a4139574193637ac50f5d9f6f2396df3d76d
hatuna-827 <hatuna827yadaka@gmail.com>
2026-05-21 16:28:03 +0900

    add PFAS

--- do.sh
+++ do.sh
@@ -1,2 +1,1 @@
-# /usr/bin/python3 /home/hatuna-827/files/github/nonogram_solver/main.py < /home/hatuna-827/files/github/nonogram_solver/example/pinz_40_40.txt
-/usr/bin/python3 /home/hatuna-827/files/github/nonogram_solver/main.py < /home/hatuna-827/files/github/nonogram_solver/example/face_5_5.txt
+python main.py < example/face_5_5.txt
